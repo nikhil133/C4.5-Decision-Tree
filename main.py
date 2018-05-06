387b42a95f63c3e6edaf298a2d24e75f08528f84
nikhil133 <nikn135@gmail.com>
2018-05-06 16:07:06 +0530

    Handles discrete values no need of encoding
    commit from generic branch
    Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -2,32 +2,34 @@
 import pdb
 import csv
 from c45 import C45
-#pdb.set_trace()
+
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder #install necessary package
 from os.path import basename,splitext
 
-filename="pgwebwln.csv" 
+filename="playable.csv"
 fname=splitext(basename(filename))
 fileprefix=fname[0]
 fname=fileprefix+'.data'
 dataset=pd.read_csv(filename)
 reader=csv.reader(open(filename),delimiter=',')
-counter=0
+
+#counter=0
 x=dataset.iloc[:,:].values
+'''
 i=0
 
 for row in reader:
 	if counter==1:
-		firstrow=row
+		l=row
 		break
 	else:
 		counter+=1			
 i=0
 encodeindex=[]
-for col in firstrow:
-	if firstrow.index(col)<len(firstrow)-1 and not col.isdigit():
+for col in l:
+	if l.index(col)<len(l)-1 and not col.isdigit():
 				encodeindex.append(i)
 	i+=1
 
@@ -36,9 +38,10 @@ for i in encodeindex:
 	x[:,i]=le.fit_transform(x[:,i])
 
 np.savetxt(fname,x,fmt='%s',delimiter=',')
-
+'''
 c1 = C45(fileprefix+".data", fileprefix+".names")
 c1.fetchData()
 c1.preprocessData()
+#pdb.set_trace()
 c1.generateTree()
 c1.printTree()
